67064a00666c4a3f10f8eec6fd69bd7d6222fae2
Abhiram Akella <59904365+abhiramakella@users.noreply.github.com>
2023-10-25 19:49:43 -0400

    Update lab 6.py

--- lab 6.py
+++ lab 6.py
@@ -8,6 +8,27 @@ def encode_password(password):
         enc_password += str(encoded_digit)  # the digit will become a string
     return enc_password
 
+def decode(encoded_string):
+    decoded_string = ""
+    for i in range(0, len(encoded_string)):
+        try:
+            temp = int(encoded_string[i])
+            if temp < 3:
+                print("Invalid encoded string")
+                decoded_string =""
+                break
+            decoded_string += str(temp-3)
+        except:
+            print("Invalid encoded string")
+            decoded_string = ""
+            break
+    return decoded_string
+encode_decode_menu = ("Menu\n"
+                      "-------------\n"
+                      "1. Encode\n"
+                      "2. Decode\n"
+                      "3. Quit")
+print(encode_decode_menu)
 
 if __name__ == '__main__':
     enc_password = ""
